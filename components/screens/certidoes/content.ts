d638023f500c94268f00a9e6fbc9563647de18e5
Josué Juca <josuejuca19@gmail.com>
2026-03-11 18:51:23 -0300

    feat(ui): criação da preview das certidoes

---
+++ components/screens/certidoes/content.ts
@@ -0,0 +1,8 @@
+export const CertidoesContent = {
+  title: 'Emissão de certidões',
+  previewActionLabel: 'Solicitar nova emissão',
+  previewActionLabelTwo: 'Minhas emissões',
+  previewText:
+    'Ao solicitar uma emissão de certidões da imoGo, você recebe, em até um dia útil, todas as certidões relativas a situação jurídica do proprietário e do imóvel, necessárias para a segurança da transação e o fechamento da venda.',
+  
+} as const;
