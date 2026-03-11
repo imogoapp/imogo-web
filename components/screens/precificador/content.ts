a0acb817101e9f0899f55f61a8f1eccb21095079
Josué Juca <josuejuca19@gmail.com>
2026-03-11 18:32:58 -0300

    feat(ui): criação da preview do precificador

---
+++ components/screens/precificador/content.ts
@@ -0,0 +1,7 @@
+export const precificadorContent = {
+  title: 'Precificador imobiliário',
+  previewActionLabel: 'Gerar laudo avaliativo',
+  previewText:
+    'lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti distinctio reprehenderit sint aspernatur, perferendis voluptatibus ullam culpa molestiae ex eveniet architecto magnam reiciendis molestias adipisci, quod impedit laudantium saepe repellendus!',
+  
+} as const;
