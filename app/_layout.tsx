cfa85cf5f8b8895d6b13edabd8300727c0fff4b7
Josué Juca <josuejuca19@gmail.com>
2026-08-18 16:34:08 -0300

    fix(web): mover script do Microsoft Clarity para o head do +html.tsx Fixes #41

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -127,19 +127,6 @@ export default function RootLayout() {
         <meta property="og:site_name" content="imoGo" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="apple-touch-icon" href="/img/favicon.png" />
-        {/* Clarity Analytics */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "y4f3vejlhr");
-            `,
-          }}
-        />
       </Head>
 
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
